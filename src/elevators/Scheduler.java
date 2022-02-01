590e6cef83566dd558dc9c1794ce0ac22253a975
unknown <evanpelletier3@cmail.carleton.ca>
2022-02-01 15:16:42 -0500

    Adding commands, overloading constructor

--- src/elevators/Scheduler.java
+++ src/elevators/Scheduler.java
@@ -1,20 +1,44 @@
 package elevators;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
+
+/*
+ * Class for the Scheduler, contains synchronized methods for queue
+ */
 public class Scheduler implements Runnable {
 	
 	private Queue<Message> commandQueue;
 	
+	/*
+	 * Create new instance of Scheduler
+	 */
+	public Scheduler() {
+		this.commandQueue = new LinkedList<Message>();
+	}
+	
+	/*
+	 * Create a new instance of Scheduler
+	 * @param Queue of messages
+	 */
 	public Scheduler(Queue<Message> commandQueue) {
 		this.commandQueue = commandQueue;
 	}
 	
+	/*
+	 * Push a new command to the commandQueue
+	 * @param Message to be added
+	 */
 	public synchronized void addCommand(Message msg) {
 		commandQueue.add(msg);
 		notifyAll();
 	}
 	
+	/*
+	 * Get latest command from the commandQueue
+	 * @return Latest command from the message queue
+	 */
 	public synchronized Message getCommand() {
 		
 		while(commandQueue.isEmpty()) {
