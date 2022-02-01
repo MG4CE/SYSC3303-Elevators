74c566471d5286e139788d9693eded1eaadce779
unknown <maged.othm@gmail.com>
2022-02-01 17:04:48 -0500

    Updated Command to handle all expected command inputs

--- src/elevators/Command.java
+++ src/elevators/Command.java
@@ -4,7 +4,7 @@ import java.util.Calendar;
 
 public class Command {
 	
-	enum Direction {
+	public enum Direction {
 		UP,
 		DOWN;
 	}
@@ -12,17 +12,46 @@ public class Command {
 	private Calendar time;
 	private int floor;
 	private Direction direction;
-	private int elevatorID;
+	private int selectedFloor;
 	
-	public Command(Calendar time, int floor, Direction direction, int elevatorID) {
-		this.time = time;
+	public Command(String time, int floor, String direction, int selectedFloor) throws NumberFormatException, IllegalArgumentException {
+		this.time = stringToCalendar(time);
 		this.floor = floor;
-		this.direction = direction;
-		this.elevatorID = elevatorID;
+		this.direction = stringToDirection(direction);
+		this.selectedFloor = selectedFloor;
+	}
+	
+	private Calendar stringToCalendar(String time) throws NumberFormatException, IllegalArgumentException {
+		String timeParts[] = time.split("[:.]");
+		
+		if (timeParts.length != 4) {
+			throw new IllegalArgumentException("Time string is improperly formated");
+		}
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR, Integer.parseInt(timeParts[0]));
+		cal.set(Calendar.MINUTE, Integer.parseInt(timeParts[1]));
+		cal.set(Calendar.SECOND, Integer.parseInt(timeParts[2]));
+		cal.set(Calendar.MILLISECOND, Integer.parseInt(timeParts[3]));
+		return cal;
+	}
+	
+	private Direction stringToDirection(String direction) {
+		if (direction.equals("Up")) {
+			return Direction.UP;
+		} else if (direction.equals("Down"))  {
+			return Direction.DOWN;
+		}
+		
+		throw new IllegalArgumentException("Invalid direction string");
+	}
+	
+	public Calendar getTime() {
+		return time;
 	}
 	
 	public String getTimestamp() {
-		return String.format("%i:%i:%i:%i", 
+		return String.format("%i:%i:%i.%i", 
 				time.get(Calendar.HOUR),
 				time.get(Calendar.MINUTE),
 				time.get(Calendar.SECOND),
@@ -37,7 +66,7 @@ public class Command {
 		return direction;
 	}
 	
-	public int getElevatorID() {
-		return elevatorID;
+	public int getselectedFloor() {
+		return selectedFloor;
 	}
 }
