050d0444d63f1ada69a5c1ddbedb725d7277ea54
Kevin Fox <kevindafox@gmail.com>
2022-02-01 17:02:18 -0500

    Touched up elevator

--- src/elevators/Elevator.java
+++ src/elevators/Elevator.java
@@ -2,22 +2,37 @@ package elevators;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Queue;
+import java.util.Calendar;
+
 
 import static java.util.Objects.isNull;
 
 public class Elevator implements Runnable {
 	
-	private int floor;
-	private Date time; 
-	private boolean available;
+	private int floor; 
 	private boolean finished;
 	private final Scheduler theScheduler;
 	
 	
+	/**
+	 * Basic constructor
+	 * @param theScheduler
+	 */
 	public Elevator(Scheduler theScheduler) {
+		this.floor = 0;
 		this.theScheduler = theScheduler;
+		this.finished = false;
+	}
+	/**
+	 * Constructor for tests
+	 * @param floor
+	 * @param scheduler
+	 * @param finished
+	 */
+	public Elevator(int floor, Scheduler scheduler,  boolean finished) {
+		this.floor = floor;
+		this.theScheduler = scheduler;
+		this.finished = finished;
 	}
 	
 	
@@ -30,30 +45,33 @@ public class Elevator implements Runnable {
 		
 	}
 	
+	/**
+	 * Main function for the elevator to run with
+	 */
 	private void runElevator() {
-		if(available) {
-			Command command = theScheduler.getCommand();
-			if(!isNull(command)) {
-			goToFloorAndSetTime(command);
-			}
+			
+		Command command = theScheduler.getCommand();
+		if(!isNull(command)) {
+			goToFloor(command);
 		}
 	}
 	
-	private void goToFloorAndSetTime(Command message) {
-		setFloor(message.getFloor());
-		SimpleDateFormat formatter6=new SimpleDateFormat("hh:mm:ss.mmm"); 
-		try {
-			setTime(formatter6.parse(message.getTimestamp()));
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		//This is to simulate
+	/**
+	 * Once a command is gotten, go to the next floor
+	 * @param command
+	 */
+	private void goToFloor(Command command) {
+		setFloor(command.getFloor());
+		
+		//This is to simulate the elevator moving
 		try {
 			Thread.sleep(100);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.printf("I am Elevator and am going to floor %d and the time is %s",
+														command.getFloor(), 
+														command.getTimeStamp());
 		
 	}
 	
@@ -63,17 +81,6 @@ public class Elevator implements Runnable {
 	public void setFloor(int floor) {
 		this.floor = floor;
 	}
-	public Date getTime() {
-		return time;
-	}
-	public void setTime(Date time) {
-		this.time = time;
-	}
-	public boolean isAvailable() {
-		return available;
-	}
-	public void setAvailable(boolean available) {
-		this.available = available;
-	}
+	
 
 }
