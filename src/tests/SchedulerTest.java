bfbd49e6a97fba3f16fb2815f5070fe29d7c1531
unknown <evanpelletier3@cmail.carleton.ca>
2022-02-01 16:59:49 -0500

    adding test cases for main classes

--- src/tests/SchedulerTest.java
+++ src/tests/SchedulerTest.java
@@ -2,44 +2,45 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import org.junit.jupiter.api.Test;
 
+import elevators.Command;
 import elevators.Scheduler;
 
 class SchedulerTest {
 	private Scheduler testScheduler;
 	
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-		
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-		
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
 	@Test
-	void testCreateScheduler() {
+	void testCreateSchedulerDefault() {
 		testScheduler = new Scheduler();
 		assertNotNull(testScheduler);
 	}
 	
-	void testAddCommand() {
-		Command cmd = new Command();
-		testScheduler.addCommand(null);
+	@Test
+	void testCreateSchedulerWithQueue() {
+		Queue<Command> commandQueue = new LinkedList<Command>(); // use empty queue
+		testScheduler = new Scheduler(commandQueue);
+		assertNotNull(testScheduler);
+		assertTrue(this.testScheduler.getCommandQueueSize() == 0);
 	}
 
+	@Test
+	void testAddCommand() {
+		Command cmd = new Command(Calendar.getInstance(), 1, Command.Direction.UP, 123);
+		assertNotNull(cmd);
+		this.testScheduler.addCommand(cmd);
+		assertTrue(this.testScheduler.getCommandQueueSize() == 1); // should have 1 item in 
+	}
+	
+	@Test
+	void testRemoveCommand() {
+		Command cmd = this.testScheduler.getCommand();
+		assertTrue(this.testScheduler.getCommandQueueSize() == 0); // should be empty
+		assertNotNull(cmd);
+	}
+	
 }
