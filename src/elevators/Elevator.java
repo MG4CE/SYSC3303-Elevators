85d233599cf86710e49d315914d1543170f54614
Kevin Fox <kevindafox@gmail.com>
2022-02-01 14:25:02 -0500

    Elevator is done no comments

--- src/elevators/Elevator.java
+++ src/elevators/Elevator.java
@@ -1,5 +1,66 @@
 package elevators;
 
-public class Elevator {
+import java.util.Date;
 
+public class Elevator implements Runnable {
+	
+	private int floor;
+	private Date time; 
+	private boolean available;
+	private boolean finished;
+	private final Scheduler theScheduler;
+	
+	
+	public Elevator(Scheduler theScheduler) {
+		this.theScheduler = theScheduler;
+	}
+	
+	
+	@Override
+	public void run() {
+		
+		while(!finished) {
+			runElevator();
+		}
+		
+	}
+	
+	private void runElevator() {
+		if(available) {
+			goToFloorAndSetTime(theScheduler.getMessage());
+		}
+	}
+	
+	private void goToFloorAndSetTime(Message message) {
+		setFloor(message.getFloor());
+		setTime(message.getTime());
+		//This is to simulate
+		Thread.sleep(100);
+		
+	}
+	
+	public int getFloor() {
+		return floor;
+	}
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+	public Date getTime() {
+		return time;
+	}
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	public boolean isAvailable() {
+		return available;
+	}
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	
+	
+	
+	
+	
 }
