3e139fb3c58257fa67f14874130de488560f36cb
unknown <maged.othm@gmail.com>
2022-02-18 00:12:50 -0500

    added a LightStatus field to FloorDirectionMessage

--- src/commands/FloorDirectionMessage.java
+++ src/commands/FloorDirectionMessage.java
@@ -1,17 +1,20 @@
 package commands;
 
 import java.util.Calendar;
+import components.LightStatus;
 
 import elevators.Direction;
 
 public class FloorDirectionMessage extends Command {
 	private Direction direction;
 	private int floorNum;
+	private LightStatus lightStatus;
 	
-	public FloorDirectionMessage(Direction direction, int floorNum) {
+	public FloorDirectionMessage(Direction direction, int floorNum, LightStatus lightStatus) {
 		super(Calendar.getInstance());
 		this.direction = direction;
 		this.floorNum = floorNum;
+		this.lightStatus = lightStatus;
 	}
 	
 	public Direction getDirection() {
@@ -20,5 +23,9 @@ public class FloorDirectionMessage extends Command {
 	
 	public int getFloor() {
 		return floorNum;
+	}
+	
+	public LightStatus getLightStatus() {
+		return lightStatus;
 	}
 }
