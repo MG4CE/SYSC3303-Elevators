90d133c1eef43b78c30fd64f7f1331f93cfd09df
unknown <evanpelletier3@cmail.carleton.ca>
2022-02-19 02:25:22 -0500

    adding elevator fsm

--- src/elevators/Elevator.java
+++ src/elevators/Elevator.java
@@ -6,153 +6,153 @@ import commands.Command;
 import commands.ElevatorArrivedMessage;
 import commands.ElevatorMovingMessage;
 import commands.ElevatorRequestCommand;
-import scheduler.Scheduler;
-import stateMachines.ElevatorFSM;
-import stateMachines.ElevatorFSM.State;
+import commands.ElevatorSensorMessage;
+
 
 public class Elevator implements Runnable {
-	// instance variables
-	Motor motor;
-	ElevatorFSM fsm;
+	// FSM State Variables
+	public enum State{IDLE, BOARDING, MOVING, ARRIVING};
+	State currentState;
 	
-	// state holding variables
+	// Shared Command Variable
+	Command latestCommand;
+	Boolean readyForCommand;
+	
+	// Elevator instance variables
+	Motor motor;
+	
+	// Elevator fields
+	Boolean running;
 	int currentFloor;
 	Direction currentDirection;
 	int destinationFloor;
-	Scheduler scheduler;
+
 	
-	Command command;
-	
-	
-	
-	public Elevator(Scheduler scheduler){
-		fsm = new ElevatorFSM();
+	public Elevator(){
+		this.currentState = State.IDLE;
 		currentFloor = 0;
 		destinationFloor = 0;
 		motor = new Motor();
 		currentDirection = Direction.IDLE;
-		command = null;
-		this.scheduler = scheduler;
+		latestCommand = null;
+		readyForCommand = true;
+		running = true;
 	}
 	
-	public int getCurrentFloor() {
-		return this.currentFloor;
-	}
-	
-	//TODO REMOVE AFTER WORKING, HERE FOR TESTS!
-	public void setCurrentFloor(int floor) {
-		this.currentFloor = floor;
+	public State getCurrentState() {
+		return currentState;
 	}
 	
 	public int getDestinationFloor() {
-		return this.destinationFloor;
+		return destinationFloor;
 	}
 	
 	public void setDestinationFloor(int floor) {
-		this.destinationFloor = floor;
+		destinationFloor = floor;
 	}	
 	
 	public Direction getDirection() {
-		return this.currentDirection;
+		return currentDirection;
 	}
 	
 	public void setDirection(Direction direction) {
-		this.currentDirection = direction;
+		currentDirection = direction;
 	}
-	
-	public ElevatorFSM.State getElevatorState(){
-		return fsm.getState();
-	}
-	
-	public synchronized void setCurrentCommand(Command cmd) {
-		this.command = cmd;
+
+	public void setCurrentFloor(int floor) {
+		currentFloor = floor;
 	}
 	
 	
-	
-	
-	
-	/*
-	 * Wait for the latest command, add synchronize wait
-	 */
-	public synchronized void onCommand(Command cmd) {
-		ElevatorFSM.State prevState = fsm.getState();
-		ElevatorFSM.State s = fsm.nextState(cmd, this);
-		switch (s){
-		case INIT :
-			//This case shouldnt really happen
-			break;
-		case IDLE :
-			//We get a new command from the shceduler or we wait
-			//Blank command to wait for another one
-			this.command = null;
-			
-			break;
-		case BOARDING:
-			doorOpen();
-			try {
-				//Let people get on
-				Thread.sleep(30);
-			}catch(Exception e) {}
-			doorClose();
-			
-			try {
-				wait(300);
-			}catch (InterruptedException e) {
-				//No button press therefore go back to idle and wait for another command
-				fsm.nextState(null, this);
-				this.command = null;
-			}
-			
-			//wait for 30s
-			break;
-		case MOVING:
-			if(prevState.equals(State.IDLE)) {
-				notifySchedulerOfMessage(new ElevatorMovingMessage(getCurrentFloor(), 123));
-			}
-			this.motor.move(currentDirection);
-			
-			
-			break;
-		case ARRIVING :
-			this.motor.stopMotor();
-			notifySchedulerOfMessage(new ElevatorArrivedMessage(getCurrentFloor(), 123));
-			
-			
-			break;
-		default:
-			break;
-		}
-	}
-	
-	public void notifySchedulerOfMessage(Command cmd) {
-		this.scheduler.ElevatorReply(cmd, this);
-	}
-	
-	public void notifySchedulerOfChangeDestination() {
-		
-	}
-	
-	private void doorOpen() {
-		System.out.println("Elevator Doors openning");
-	}
-	private void doorClose() {
-		System.out.println("Elevator Doors Closing");
-	}
-	
-	
+	// FSM Shit
 	@Override
 	public void run() {
-		while(true) {
-			while(this.command != null) {
-				try {
-					wait();
-				}catch(Exception e) {}
-			}
-			onCommand(this.command);
+		while(running) {
+			updateFSM(getLatestCommand());
 		}
-		
 	}
 	
+	public synchronized void elevatorPutCommand(Command command) {
+		while(!readyForCommand) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				System.out.print("something failed???");
+			}
+		}
+		latestCommand = command;
+		readyForCommand = false;
+		notifyAll();
+	}
 	
+	private synchronized Command getLatestCommand() {
+		while(readyForCommand) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				System.out.print("Fuck you");
+			}
+		}
+		readyForCommand = true;
+		notifyAll();
+		return latestCommand;
+	}
+			
+	/*
+	 * FSM IMPLEMETNATION
+	 */
+	public synchronized void updateFSM(Command command) {
+		switch (this.currentState){
+		case IDLE :
+			if(command instanceof ElevatorRequestCommand) {
+				ElevatorRequestCommand c = (ElevatorRequestCommand) command;
+				if(this.currentFloor == c.getFloor()) {
+					this.currentState = State.BOARDING;
+					return;
+				}else {
+					currentState = State.MOVING;
+				}
+			}
+			break;
+			
+		case BOARDING:
+			//try {
+			//	wait(30 * 1000); //TODO COME BACK
+			//} catch (InterruptedException e) {
+			//	if (currentState == State.BOARDING) 
+			//		this.currentState = State.IDLE;
+			//}
+			// check if button on other floor was clicked
+			if (command instanceof ElevatorRequestCommand) {
+				this.currentState = State.IDLE;
+			}
+			break;
+	
+		
+		case MOVING:
+			// check if a higher priority stop was sent by the scheduler, back to MOVING
+			if (command instanceof ElevatorRequestCommand) {
+				ElevatorRequestCommand c = (ElevatorRequestCommand) command;
+				setDestinationFloor(c.getFloor()); // update latest destination
+				currentState = State.MOVING;
+			} 
+			// Any time elevator passes a floor
+			if(command instanceof ElevatorSensorMessage) {
+				ElevatorSensorMessage c = (ElevatorSensorMessage) command;
+				setCurrentFloor(c.getFloor());
+				if(currentDirection == Direction.UP && currentFloor == destinationFloor -1) {
+					currentState = State.ARRIVING;
+				}else if(currentDirection == Direction.DOWN && currentFloor == destinationFloor +1) {
+					currentState = State.ARRIVING;
+				}
+			}
+			break;
+			
+		case ARRIVING :
+			if(command instanceof ElevatorSensorMessage) {
+				currentState = State.BOARDING;
+			}
+			break;
+		}		
+	}	
 }
--- src/tests/ElevatorTest.java
+++ src/tests/ElevatorTest.java
@@ -1,98 +1,43 @@
 package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import commands.ElevatorMovingMessage;
 import commands.ElevatorRequestCommand;
 import commands.ElevatorSensorMessage;
-import elevators.Direction;
 import elevators.Elevator;
-import stateMachines.ElevatorFSM;
-import stateMachines.ElevatorFSM.State;
 
 class ElevatorTest {
 	Elevator elevator;
 	
 	@BeforeEach
-    public void init() {
-		elevator = new Elevator(null);
-    }
+	void init() {
+		elevator = new Elevator();
+	}
 	
-	
-	/*
-	 * Test if FSM goes from idle to boarding
-	 */
 	@Test
-	void testTransitionIdleToBoarding() {
-		ElevatorRequestCommand cmd = new ElevatorRequestCommand("00:00:00.0", 0, 123);
-		elevator.onCommand(cmd); // Attempt to put FSM in BOARDING state
-		assertEquals(State.BOARDING, elevator.getElevatorState());
+	void testEnterBoardingState() {
+		ElevatorRequestCommand cmd = new ElevatorRequestCommand("0:0:0.1", 0, 0);
+		elevator.updateFSM(cmd);
+		assertEquals(Elevator.State.BOARDING, elevator.getCurrentState());
 	}
 
-	/*
-	 * Test if FSM goes from boarding to idle
-	 */
 	@Test
-	void testTransitionBoardingToIdle() {
-		testTransitionIdleToBoarding();
-		ElevatorRequestCommand c2 = new ElevatorRequestCommand("00:00:00.0", 2, 123);
-		elevator.onCommand(c2); // Attempt to put FSM in IDLE state
-		assertEquals(State.IDLE, elevator.getElevatorState());
+	void testEnterMovingState() {
+		ElevatorRequestCommand cmd = new ElevatorRequestCommand("0:0:0.1", 10, 0);
+		elevator.updateFSM(cmd);
+		assertEquals(Elevator.State.MOVING, elevator.getCurrentState());
 	}
-	
-	/*
-	 * Test if FSM goes to moving state
-	 */
-	@Test
-	void testTransitionIdleToMoving() {
-		int destFloor = 10;
-		ElevatorRequestCommand c1 = new ElevatorRequestCommand("00:00:00.0", destFloor, 123);
-		elevator.onCommand(c1); // Attempt to put FSM in moving state
-		assertEquals(elevator.getDestinationFloor(), destFloor);
-		assertEquals(State.MOVING, elevator.getElevatorState());
-	}
-	
-	/*
-	 * Test if FSM updates floor if Higher priority request comes in
-	 */
-	@Test
-	void testHighPriorityRequestMoving() {
-		int destFloor = 5;
-		testTransitionIdleToMoving(); // set elevator to move towards floor 10
-		ElevatorRequestCommand c1 = new ElevatorRequestCommand("00:00:00.0", destFloor, 123);
-		// send floor in middle
-		elevator.onCommand(c1);
-		assertEquals(elevator.getDestinationFloor(), destFloor);
-		assertEquals(State.MOVING, elevator.getElevatorState());
-	}
-	
-	/*
-	 * Test if FSM updates from moving to ARRIVING state
-	 */
-	@Test
-	void testTransitionMovingToArriving() {
-		testTransitionIdleToMoving();
-		int floor = elevator.getDestinationFloor() -1; // one floor under destination floor
-		elevator.setCurrentFloor(floor);
-		ElevatorSensorMessage cmd = new ElevatorSensorMessage(floor);
-		elevator.onCommand(cmd);
-		assertEquals(State.ARRIVING, elevator.getElevatorState());
-	}
-	
-	@Test
-	void testTransitionArrivingToBoarding() {
-		testTransitionMovingToArriving();
-		int floor = elevator.getDestinationFloor();
-		elevator.setCurrentFloor(floor);
-		ElevatorSensorMessage cmd = new ElevatorSensorMessage(floor);
-		elevator.onCommand(cmd);
-		assertEquals(State.BOARDING, elevator.getElevatorState());
-	}
-	
-	
-	
 
+	@Test
+	void testEnterArriving() {
+		testEnterMovingState();
+		elevator.setCurrentFloor(10);
+		ElevatorSensorMessage cmd = new ElevatorSensorMessage(0);
+		elevator.updateFSM(cmd);
+		assertEquals(Elevator.State.MOVING, elevator.getCurrentState());
+	}
 	
 }
