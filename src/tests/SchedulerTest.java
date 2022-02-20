1ef288239c197987e96d83a9f2be47ed9ec2abf7
unknown <evanpelletier3@cmail.carleton.ca>
2022-02-19 23:43:56 -0500

    adding tests and images

--- src/tests/SchedulerTest.java
+++ src/tests/SchedulerTest.java
@@ -5,8 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import commands.ExternalFloorBtnCommand;
+import commands.InteriorElevatorBtnCommand;
+import elevators.Direction;
 import elevators.Elevator;
 import scheduler.Scheduler;
+import scheduler.Scheduler.controlState;
 
 class SchedulerTest {
 	Scheduler scheduler;
@@ -14,11 +18,37 @@ class SchedulerTest {
 	
 	@BeforeEach
 	void init() {
+		scheduler = new Scheduler();
+		elevator = new Elevator(scheduler, 123);
+		scheduler.setElevator(elevator);
 	}
 	
 	@Test
-	void testEnterDispatchState() {
-
+	void testElevatorAdded() {
+		assertTrue(scheduler.hasElevator());
 	}
+	
+	@Test
+	void testExternalButtonClick() {
+		ExternalFloorBtnCommand cmd = new ExternalFloorBtnCommand(2, Direction.UP, 123); 
+		scheduler.updateControlFSM(cmd);
+		assertEquals(controlState.DISPATCH, scheduler.getState());
+	}
+	
+	@Test
+	void testInternalButtonClick() {
+		InteriorElevatorBtnCommand cmd = new InteriorElevatorBtnCommand(2, 123); 
+		scheduler.updateControlFSM(cmd);
+		assertEquals(controlState.DISPATCH, scheduler.getState());
+	}
+	
+	@Test
+	void testFloorAdded() {
+		InteriorElevatorBtnCommand cmd = new InteriorElevatorBtnCommand(2, 123); 
+		scheduler.updateControlFSM(cmd);
+		assertEquals(2, scheduler.getNextFloor());
+	}
+	
+	
 
 }
