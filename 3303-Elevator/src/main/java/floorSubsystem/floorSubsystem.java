30cad0ea49945d1d3520cbade2dd922e3f10e3a2
robotevan <robotevan24@gmail.com>
2022-03-08 21:55:49 -0500

    adding pbhelper to subsystem

--- 3303-Elevator/src/main/java/floorSubsystem/floorSubsystem.java
+++ 3303-Elevator/src/main/java/floorSubsystem/floorSubsystem.java
@@ -1,4 +1,23 @@
 package floorSubsystem;
 
-public class floorSubsystem {
+import elevators.Elevator;
+import pbHelpers.PbMessage;
+import pbHelpers.UdpPBHelper;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.SocketException;
+
+public class floorSubsystem extends UdpPBHelper{
+    private int NUM_LINES = 8; // rodrigo do this
+
+    floorSubsystem(Elevator elevator) throws SocketException {
+        super(0,0);
+
+    }
+
+    void start(){
+        for(int i = 0; i < NUM_LINES; i++){
+        }
+    }
 }
