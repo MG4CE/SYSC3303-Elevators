39197d2e829e847b8c8cb2de9a83b7c619a2b152
robotevan <robotevan24@gmail.com>
2022-03-10 08:28:21 -0500

    adding comments to UDPHelper

--- 3303-Elevator/src/main/java/protoBufHelpers/UDPHelper.java
+++ 3303-Elevator/src/main/java/protoBufHelpers/UDPHelper.java
@@ -10,27 +10,46 @@ import java.util.logging.Logger;
 
 import elevatorCommands.*;
 
-
+/*
+ * Helper class to be extended by any class that sends/receives messages
+ */
 public abstract class UDPHelper {
 	private final Logger LOGGER = Logger.getLogger(UDPHelper.class.getName());
-	private DatagramSocket recvSocket;
+	private final DatagramSocket recvSocket;
 	private final int PACKET_SIZE = 1024;
-	
+
+	/*
+	 * Constructor to be called with super(), will create a listen socket
+	 */
 	public UDPHelper() throws SocketException{
 		this.recvSocket = new DatagramSocket();
 	}
-	
+
+	/*
+	 * Constructor to be called with super(), will create a listen socket bound to recvPort
+	 * @param port to listen to
+	 */
 	public UDPHelper(int recvPort) throws SocketException{
 		this.recvSocket = new DatagramSocket(recvPort);
 	}
-	
-	public void sendByteArray(byte[] pbMsg, int sendPort) throws IOException {
-		DatagramPacket sendPacket = new DatagramPacket(pbMsg, pbMsg.length, InetAddress.getLocalHost(), sendPort);
+
+	/*
+	 * Send a raw byte array to a destination port
+	 * @param data byte array to be send via udp
+	 * @param port to send to
+	 */
+	public void sendByteArray(byte[] data, int sendPort) throws IOException {
+		DatagramPacket sendPacket = new DatagramPacket(data, data.length, InetAddress.getLocalHost(), sendPort);
 		DatagramSocket sendSocket = new DatagramSocket();
 		sendSocket.send(sendPacket);
 		sendSocket.close();
 	}
-	
+
+	/*
+	 * Send a message of type protobufmessage (Any of defined messages) Will wrap message in WrapperMessage first
+	 * @param Protobuf message to send
+	 * @param port to send message to
+	 */
 	public void sendMessage(com.google.protobuf.GeneratedMessageV3 message, int sendPort) throws IOException {
 		LOGGER.info("Sending Protobuf of type " + message.getClass().getName());
 		WrapperMessage.Builder msgBldr = WrapperMessage.newBuilder();
@@ -53,7 +72,11 @@ public abstract class UDPHelper {
 		WrapperMessage msg = msgBldr.build();
 		sendByteArray(msg.toByteArray(), sendPort);
 	}	
-	
+
+	/*
+	 * Blocking call to receive a message, returns it datagram packet
+	 * @return datagram packet received from listen socket
+	 */
 	public DatagramPacket receiveMessage() throws IOException {
 		byte[] r = new byte[PACKET_SIZE];
 		DatagramPacket rcv = new DatagramPacket(r, PACKET_SIZE);
@@ -62,6 +85,9 @@ public abstract class UDPHelper {
 		return rcv;
 	}
 
+	/*
+	 * Close listen socket
+	 */
 	public void closePbSocket(){
 		this.recvSocket.close();
 	}
