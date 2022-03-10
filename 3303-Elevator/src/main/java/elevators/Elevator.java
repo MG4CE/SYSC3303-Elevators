7cd49d4f7e2e2cb2f0848d731fdb5e00380be031
robotevan <robotevan24@gmail.com>
2022-03-10 08:40:13 -0500

    adding comments to elevator, adding more verbosity to actions

--- 3303-Elevator/src/main/java/elevators/Elevator.java
+++ 3303-Elevator/src/main/java/elevators/Elevator.java
@@ -33,7 +33,10 @@ public class Elevator extends UDPHelper implements Runnable {
 		OPEN,
 		CLOSE,
 	}
-	
+
+	/*
+	 * Constructor for elevator class, takes in the scheduler to send to, as well as the port it should listen to
+	 */
 	public Elevator(int schedulerPort, int receivePort) throws SocketException {
 		super(receivePort); //takes in a receive port just for testing
 		this.schedulerPort = schedulerPort;
@@ -45,6 +48,10 @@ public class Elevator extends UDPHelper implements Runnable {
 		this.running = true;
 	}
 
+	/*
+	 * Send a UDP message indicating elevator had internal button pressed
+	 * @param floor number of button pressed
+	 */
 	protected void sendInternalButtonMessage(int floor) throws IOException {
 		ElevatorRequestMessage msg = ElevatorRequestMessage.newBuilder()
 				.setFloor(floor)
@@ -56,7 +63,9 @@ public class Elevator extends UDPHelper implements Runnable {
 		sendMessage(msg, schedulerPort);
 	}
 
-	// Should this include direction?
+	/*
+	 * Send a UDP message indicating elevator is passing a floor
+	 */
 	protected void sendFloorSensorMessage() throws IOException {
 		FloorSensorMessage msg = FloorSensorMessage.newBuilder()
 				.setFloor(this.currentFloor)
@@ -66,6 +75,9 @@ public class Elevator extends UDPHelper implements Runnable {
 		sendMessage(msg, schedulerPort);
 	}
 
+	/*
+	 * Send a UDP message indicating elevator is departing a floor towards a destination
+	 */
 	protected void sendDepartureMessage() throws IOException {
 		ElevatorDepartureMessage msg = ElevatorDepartureMessage.newBuilder()
 				.setDirection(this.currentDirection)
@@ -76,6 +88,9 @@ public class Elevator extends UDPHelper implements Runnable {
 		sendMessage(msg, schedulerPort);
 	}
 
+	/*
+	 * Send a UDP message indicating elevator has arrived at a floor
+	 */
 	protected void sendElevatorArrivedMessage() throws IOException {
  		ElevatorArrivedMessage msg = ElevatorArrivedMessage.newBuilder()
 				.setElevatorID(this.elevatorID)
@@ -85,6 +100,9 @@ public class Elevator extends UDPHelper implements Runnable {
 		sendMessage(msg, schedulerPort);
 	}
 
+	/*
+	 * Run method to start an elevator after creating a new instance of it
+	 */
 	@Override
 	public void run() {
 		while(this.running){
@@ -99,55 +117,96 @@ public class Elevator extends UDPHelper implements Runnable {
 		}
 	}
 
+	/*
+	 * To be called by elevator motor, updates floor after an interval has passed
+	 */
 	protected void motorUpdate() throws IOException {
 		if(this.currentDirection == Direction.UP){
 			this.currentFloor++;
 		} else if(this.currentDirection == Direction.DOWN) {
 			this.currentFloor--;
 		}
-		LOGGER.info("Elevator Passing floor " + Integer.toString(currentFloor));
+		System.out.printf("Elevator %d passing floor %d", this.elevatorID, this.currentFloor);
 		sendFloorSensorMessage();
 		this.elevatorFSM.updateFSM(null); // poke with null message
 	}
 
+	/*
+	 * Set the destination floor to a new floor
+	 */
 	protected void setDestinationFloor(int floor) {
 		LOGGER.info("Elevator dispatched to floor " + Integer.toString(floor));
 		this.destinationFloor = floor;
 	}
 
+	/*
+	 * Get the current destination floor of the elevator
+	 * @return destination floor
+	 */
 	protected int getDestinationFloor() {
 		return this.destinationFloor;
 	}
 
+	/*
+	 * Get the current floor of the elevator
+	 * @return floor
+	 */
 	protected int getCurrentFloor() {
 		return this.currentFloor;
 	}
 
+	/*
+	 * Update the direction of the elevator based on its new destination floor
+	 */
 	protected void updateCurrentDirection() {
 		if(this.currentFloor < this.destinationFloor) {
 			this.currentDirection = Direction.UP;
-		}else {
+		}else if (this.currentFloor > this.destinationFloor) {
 			this.currentDirection = Direction.DOWN;
+		} else{
+			this.currentDirection = Direction.STATIONARY;
 		}
 	}
 
+	/*
+	 * Simulates a button press from within the elevator
+	 * @param destination floor (i.e. button pressed)
+	 */
 	public void pushElevatorButton(int floor) throws IOException {
+		System.out.printf("Elevator %d had button %d pressed\n", this.elevatorID, floor);
 		sendInternalButtonMessage(floor);
 	}
 
+	/*
+	 * Get current direction of elevator
+	 * @return Direction
+	 */
 	protected Direction getCurrentDirection() {
 		return this.currentDirection;
 	}
 
+	/*
+	 * Open the doors
+	 */
 	protected void openDoors() {
+		System.out.println("Doors opening on elevator " + Integer.toString(this.elevatorID));
 		this.currentDoorState = DoorState.OPEN;
 	}
 
+	/*
+	 * Close the doors
+	 */
 	protected void closeDoors() {
+		System.out.printf("Doors closing on elevator %d\n",  this.elevatorID);
 		this.currentDoorState = DoorState.OPEN;
 	}
 
+	/*
+	 * Check if elevator is 1 floor away from its current destination
+	 * @return true or false
+	 */
 	protected Boolean isElevatorArriving() {
+		System.out.printf("Elevator %d arriving at floor %d\n", this.elevatorID, this.destinationFloor);
 		if(this.currentDirection == Direction.UP && this.currentFloor == this.destinationFloor -1) {
 			return true;
 		}else if (this.currentDirection == Direction.DOWN && this.currentFloor == this.destinationFloor +1) {
