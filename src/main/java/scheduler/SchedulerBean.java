6d1a9c24f4378049ddc1819ef28cd6f4c96fb372
Kevin <kevindafox@gmail.com>
2022-04-04 12:52:29 -0400

    Commented my code

--- src/main/java/scheduler/SchedulerBean.java
+++ src/main/java/scheduler/SchedulerBean.java
@@ -4,27 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import message.Direction;
-import statemachine.State;
-
+/**
+ * Class to hold the current systems representation to eventually be converted to JSON
+ * @author kevin
+ *
+ */
 public class SchedulerBean {
 	
-	
-	
+	//Lists to dynamically add / delete elevator data
 	private List<Integer> elevatorList;
 	private List<Integer> elevatorAtFloorList;
 	private List<Integer> floorRequestedList;
-	private List<Integer> numberOccupentsList;
-	private int numFloors;
 	private List<Integer> elevatorDirectionList;
 	private List<Integer> elevatorStateList;
 	private List<Integer> elevatorHardFaultList;
 	
+	private int numFloors;
 	
+	//Arrays to hold List data for JSON exporting
 	private int[] elevatorArray;
 	private int[] elevatorFloorArray;
 	private int[] floorRequestedArray;
-	private int[] numberOccupentsArray;
 	private int[] elevatorDirectionArray;
 	private int[] elevatorStateArray;
 
@@ -32,37 +32,36 @@ public class SchedulerBean {
 		elevatorList = new ArrayList<>();
 		elevatorAtFloorList = new ArrayList<>();
 		floorRequestedList = new ArrayList<>();
-		numberOccupentsList = new ArrayList<>();
 		elevatorDirectionList = new ArrayList<>();
 		elevatorStateList = new ArrayList<>();
 		elevatorHardFaultList = new ArrayList<>();
 		}
 	
-	public void setElevatorList(List<Integer> elevatorList) {
-		this.elevatorList = elevatorList;
-	}
+
 	
-	public void setElevatorFloorRequest(List<Integer> elevatorFloorList) {
-		this.elevatorAtFloorList = elevatorFloorList;
-	}
+
 	
-	public void setFloorRequestedList(List<Integer> elevatorFloorRequestList) {
-		this.floorRequestedList = elevatorFloorRequestList;
-	}
-	
-	public void setNumberOccupentsList(List<Integer> elevatorOccupentsList) {
-		this.numberOccupentsList = elevatorOccupentsList;
-	}
-	
+
+	/**
+	 * Add en elevatorId that has been hard faulted
+	 * @param id
+	 */
 	public void addToHardFaults(Integer id) {
 		this.elevatorHardFaultList.add(id);
 	}
 	
+	/**
+	 * Getter for hard fault list
+	 * @return
+	 */
 	public List<Integer> getHardFaultList(){
 		return this.elevatorHardFaultList;
 	}
 	
-	public void buildArrays() {
+	/**
+	 * Takes all lists can convert to array
+	 */
+	public void buildArraysForJsonOutput() {
 		this.elevatorArray = this.elevatorList.stream()
                 .filter(Objects::nonNull)
                 .mapToInt(Integer::intValue)
@@ -77,12 +76,7 @@ public class SchedulerBean {
                 .filter(Objects::nonNull)
                 .mapToInt(Integer::intValue)
                 .toArray();
-		
-		this.numberOccupentsArray = this.numberOccupentsList.stream()
-                .filter(Objects::nonNull)
-                .mapToInt(Integer::intValue)
-                .toArray();
-		
+				
 			
 		this.elevatorDirectionArray = this.elevatorDirectionList.stream()
                 .filter(Objects::nonNull)
@@ -96,69 +90,109 @@ public class SchedulerBean {
 	}
 	
 	
+	/**
+	 * Finds the corresponding index that has the current elevatorId
+	 * Since all the data about 1 elevator uses the same index in all lists/arrays
+	 * @param elevatorId the elevatorId we are looking for
+	 * @return
+	 */
+	public int findElevatorIndex(int elevatorId) {
+		return this.elevatorList.indexOf(elevatorId);
+	}
 	
 	
+	/**
+	 * Getter for Direction List
+	 * @return
+	 */
 	public List<Integer> getDirectionList() {
 		return this.elevatorDirectionList;
 	}
 	
+	/**
+	 * Getter for state list
+	 * @return
+	 */
 	public List<Integer> getElevatorStateList(){
 		return this.elevatorStateList;
 	}
 	
+	/**
+	 * Getter for ElevatorIdList
+	 * @return
+	 */
+	public List<Integer> getElevatorList() {
+		return elevatorList;
+	}
+	/**
+	 * Getter for ElevatorCurrentFloorList
+	 * @return
+	 */
+	public List<Integer> getElevatorFloorList() {
+		return elevatorAtFloorList;
+	}
+	/**
+	 * Getter for elevator request Floor List
+	 * @return
+	 */
+	public List<Integer> getFloorRequestedList() {
+		return floorRequestedList;
+	}
+	
+	/**
+	 * Setter for number of floors
+	 * @param num
+	 */
 	public void setNumFloors(int num) {
 		this.numFloors = num;
 	}
 	
+	/**
+	 * Getter for number of floors
+	 * @return
+	 */
 	public int getNumFloors() {
 		return this.numFloors;
 	}
+	/**
+	 * Get elevatorIdArray
+	 * @return
+	 */
 	public int[] getElevatorArray() {
 		return this.elevatorArray;
 	}
 	
+	/**
+	 * Get elevatorCurrentFloorArray
+	 * @return
+	 */
 	public int[] getElevatorFloorArray() {
 		return this.elevatorFloorArray;
 	}
 	
+	/**
+	 * Get elevatorFloorRequestArray
+	 * @return
+	 */
 	public int[] getRequestedFloorArray() {
 		return this.floorRequestedArray;
 	}
 	
-	public int[] getNumberOccupentsArray() {
-		return this.numberOccupentsArray;
-	}
 	
+	/**
+	 * Get Elevator Directions array
+	 * @return
+	 */
 	public int[] getDirectionArray() {
 		return this.elevatorDirectionArray;
 	}
 	
+	/**
+	 * Get elevator states array
+	 * @return
+	 */
 	public int[] getStateArray() {
 		return this.elevatorStateArray;
 	}
 	
-	public int findElevatorIndex(int elevatorId) {
-		return this.elevatorList.indexOf(elevatorId);
-	}
-
-	public List<Integer> getElevatorList() {
-		return elevatorList;
-	}
-
-	public List<Integer> getElevatorFloorList() {
-		return elevatorAtFloorList;
-	}
-
-	public List<Integer> getFloorRequestedList() {
-		return floorRequestedList;
-	}
-
-	public List<Integer> getNumberOccupentsList() {
-		return numberOccupentsList;
-	}
-	
-	
-	
-	
-
 }
--- src/main/java/scheduler/SchedulerTCPServer.java
+++ src/main/java/scheduler/SchedulerTCPServer.java
@@ -1,48 +1,39 @@
 package scheduler;
 
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-
 import org.json.simple.JSONObject;
 
-import message.Direction;
-
+/**
+ * Class that will run as extra thread. It holds a tcp socket that can be used as an API for Front end
+ * @author kevin
+ *
+ */
 public class SchedulerTCPServer  implements Runnable {
-	private  String OUTPUT;
-	private static final String OUTPUT_HEADERS = "HTTP/1.1 200 OK\r\n" +
-	    "Content-Type: text/html\r\n" + 
-	    "Content-Length: ";
-	private static final String OUTPUT_END_OF_HEADERS = "\r\n\r\n";
-
 	private int socketNum = 107;
 	private Socket webpage;
 	private ServerSocket server;
 	private boolean running = true;
 	private Scheduler scheduler;
-	private OutputStream output;
-	BufferedWriter o;
-	SchedulerBean bean;
+	private SchedulerBean bean;
 	int numberOfElevators = 0;
 
 	
 	public SchedulerTCPServer(Scheduler scheduler) throws IOException {
 		try {
+			//Add the scheduler here
 			this.scheduler = scheduler;
+			//Start the TCP server
 			server = new ServerSocket(socketNum);
+			//Create the FrontEnd data bean
 			this.bean = new SchedulerBean();
+			//Add the tcp server to scheduler so it can comm the hard faults
 			this.scheduler.addServerToScheduler(this);
 		} catch (IOException e) {
 			
@@ -70,7 +61,11 @@ public class SchedulerTCPServer  implements Runnable {
 			}
 		}
 	}
-	
+	/**
+	 * Method used from SchedulerUtils to add to hardFaulted list
+	 * this represents what elevators have Hard Faulted
+	 * @param elevatorId
+	 */
 	protected void addToHardFaults(Integer elevatorId) {
 		this.bean.addToHardFaults(elevatorId);
 	}
@@ -85,7 +80,6 @@ public class SchedulerTCPServer  implements Runnable {
 			if((index = bean.findElevatorIndex(elevator.getElevatorID())) != -1) {
 				bean.getElevatorFloorList().set(index, elevator.getCurrentFloor());
 				bean.getFloorRequestedList().set(index, elevator.getCurrentDestination());
-				bean.getNumberOccupentsList().set(index, 1);
 				bean.setNumFloors(scheduler.numFloors);
 				bean.getElevatorStateList().set(index, translateState(elevator));
 				bean.getDirectionList().set(index, translateDirection(elevator.getCurrentDestination(), elevator.getCurrentFloor()));
@@ -95,20 +89,23 @@ public class SchedulerTCPServer  implements Runnable {
 				bean.getElevatorList().add(elevator.getElevatorID());
 				bean.getElevatorFloorList().add(elevator.getCurrentFloor()+1);
 				bean.getFloorRequestedList().add(elevator.getCurrentDestination()+1);
-				bean.getNumberOccupentsList().add(1);
 				bean.setNumFloors(scheduler.numFloors);
 				bean.getElevatorStateList().add(0);
 				bean.getDirectionList().add(0);
 				this.numberOfElevators++;
 			}
+			//Check if a hard faulted elevator and set its state to 9
 			checkForHardFaults(this.scheduler.getElevatorControl());
 		}
-		bean.buildArrays();
+		bean.buildArraysForJsonOutput();
 	}
 	
 	
+	/**
+	 * Method used to check if any elevators have hard faulted
+	 * @param elevatorControl
+	 */
 	private void checkForHardFaults(ArrayList<ElevatorControl> elevatorControl) {
-		boolean found = false;
 		int index = -1;
 		
 		for(Integer elevatorIdThatHF: this.bean.getHardFaultList()) {
@@ -118,6 +115,12 @@ public class SchedulerTCPServer  implements Runnable {
 		}
 	}
 
+	/**
+	 * Since ElevatorControl doesnt wait for the elevator to actually move
+	 * I have to translate that if the elevator Control is not on the destination floor, its moving or faulted
+	 * @param ev
+	 * @return
+	 */
 	public int translateState(ElevatorControl ev) {
 		if(ev.getState().equals(ElevatorControl.ElevatorState.STOPPED) & ev.getCurrentFloor() != ev.getCurrentDestination()) {
 			return 1;
@@ -127,6 +130,16 @@ public class SchedulerTCPServer  implements Runnable {
 		return ev.getState().ordinal();
 	}
 	
+	/**
+	 * Translate direction
+	 * up direction as 2
+	 * down direction as 1
+	 * idle as 0
+	 * 
+	 * @param destFloor
+	 * @param currFloor
+	 * @return
+	 */
 	public int translateDirection(int destFloor, int currFloor) {
 		if(destFloor > currFloor) {
 			return 2;
@@ -138,43 +151,59 @@ public class SchedulerTCPServer  implements Runnable {
 			return 0;
 	}
 	
+	/**
+	 * Rung the server
+	 * @throws IOException
+	 */
 	public void runServer() throws IOException {
+			//Method to wait for ajax call
 		    listenForCall();
+		    //Update the front end
 			updateFrontEnd();
 	}
 	
+	/**
+	 * Waits for incoming tcp call
+	 * @throws IOException
+	 */
 	private void listenForCall() throws IOException {
 		webpage = server.accept();
 	}
 	
+	/**
+	 * Update the front end by translating all system data into a json and return in an http packet
+	 * @throws IOException
+	 */
 	private void updateFrontEnd() throws IOException {
-		//Get the updated bean
+		//Update the bean
 		updateSchedulerInfo();
+		
+		//Create json for return data
 		JSONObject json = new JSONObject();
 		json.put("ids" , Arrays.toString(this.bean.getElevatorArray()));
 		json.put("floor", Arrays.toString(this.bean.getElevatorFloorArray()));
 		json.put("requestTo", Arrays.toString(this.bean.getRequestedFloorArray()));
-		json.put("occupents", Arrays.toString(this.bean.getNumberOccupentsArray()));
 		json.put("directions", Arrays.toString(this.bean.getDirectionArray()));
 		json.put("states", Arrays.toString(this.bean.getStateArray()));
 		json.put("numFloors", this.bean.getNumFloors());
 		
-		 BufferedWriter out = new BufferedWriter(new OutputStreamWriter(webpage.getOutputStream()));
-		
+		//Object outstream from the tcp server
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(webpage.getOutputStream()));
 		 	
-		 out.write("HTTP/1.0 200 OK\r\n");
-	     out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
-	     out.write("Access-Control-Allow-Origin: *\r\n");
-	     out.write("Server: Apache/0.8.4\r\n");
-	     out.write("Content-Type: text\r\n");
-	     out.write("Content-Length: "+json.toJSONString().length()+"\r\n");
-	     out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
-	     out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
-	     out.write("\r\n");
-	     out.write(json.toJSONString());
-
-	     out.flush();
-        
+		//Malform an http packet structure
+		out.write("HTTP/1.0 200 OK\r\n");
+	    out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
+	    out.write("Access-Control-Allow-Origin: *\r\n");
+	    out.write("Server: Apache/0.8.4\r\n");
+	    out.write("Content-Type: text\r\n");
+	    out.write("Content-Length: "+json.toJSONString().length()+"\r\n");
+	    out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
+	    out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
+	    out.write("\r\n");
+	    out.write(json.toJSONString());
+	    //Flush the buffer
+	    out.flush();
+        //Close the connection
 		webpage.close();
 	}
 
